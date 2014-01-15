c37693ef9f8b57f7b1459d2882e704031f88df11
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2014-01-15 12:36:39 +0100

    Updated the license of the Installer

--- Installer.php
+++ Installer.php
@@ -1,11 +1,23 @@
 #!/usr/bin/env php
 <?php
 /**
- * @license ALL RIGHTS RESERVED - Commercial use only with my approbation
- *
- * @author Jacques Bodin-Hullin <jacques@bodin-hullin.net>
  * @package Installer
- * @copyright Copyright (c) 2011-2013 Jacques Bodin-Hullin (http://jacques.sh)
+ * @copyright Copyright © 2011-2014 Jacques Bodin-Hullin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+ *
+ * @author Jacques Bodin-Hullin <@jacquesbh> <jacques@bodin-hullin.net> <http://jacques.sh>
  */
 
 declare( ticks = 1 );
